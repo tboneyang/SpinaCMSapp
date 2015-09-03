5a89abfd6b8ce5e0aafd3fa2a9a618ee087d0dd4
Thomas Lixing Yang <tboneyang@gmail.com>
2015-09-03 16:05:42 -0400

    added spina gems and installed. migrated psql tables

---
+++ config/initializers/spina.rb
@@ -0,0 +1,10 @@
+Spina::Engine.configure do
+  config.NEGATIVE_CAPTCHA_SECRET = 'bf419a91a14ff94fa6a1881e37a355c842ca1b63d9aadbdea23d3c692d4b090073302874f733124491740b6939814d12b0a49b4788e2c8fae3dd76ae37ecf953'
+
+  # If you want to use s3 to store uploads
+  # config.storage = :s3
+  # config.aws_region = "eu-west-1"
+  # config.aws_access_key_id = "abc123"
+  # config.aws_secret_key = "abc123"
+  # config.s3_bucket = "mybucket"
+end
